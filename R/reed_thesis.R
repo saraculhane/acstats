8f067c4f2d9e4105dd16902a89ac9a445f13bebe
Chester Ismay <chester.ismay@gmail.com>
2015-10-09 11:54:18 -0700

    Changed to JSS theme

--- R/reed_thesis.R
+++ R/reed_thesis.R
@@ -2,34 +2,31 @@
 reed_thesis <- function() {
   template <- find_resource("reed_thesis", "template.tex")
 
-  base <- rmarkdown::pdf_document(template = template)
-  # Render will generate tex file, post-process hook generates appropriate
-  # thesis.tex and use pandoc to build pdf from that
-  base$pandoc$to <- "latex"
-  base$pandoc$ext <- ".tex"
-
-  base$post_processor <- function(metadata, utf8_input, output_file, clean, verbose) {
-    filename <- tools::file_path_sans_ext(basename(output_file))
-    wrapper_metadata <- list(preamble = metadata$preable, filename = filename)
-    wrapper_template <- find_resource("reed_thesis", "thesis.tex")
-    wrapper_output <- file.path(getwd(), "thesis.tex")
-    template_pandoc(wrapper_metadata, wrapper_template, wrapper_output, verbose)
-
-    tools::texi2pdf("thesis.tex", clean = clean)
-    "thesis.pdf"
-  }
+  base <- rmarkdown::pdf_document(template = template, keep_tex = TRUE)
 
   # Mostly copied from knitr::render_sweave
-  base$knitr$opts_chunk$comment <- "#>"
+  base$knitr$opts_knit$out.format <- "sweave"
 
-  hook_chunk = function(x, options) {
+  base$knitr$opts_chunk$prompt <- TRUE
+  base$knitr$opts_chunk$comment <- NA
+  base$knitr$opts_chunk$highlight <- FALSE
+
+  base$knitr$opts_chunk$dev.args <- list(pointsize = 11)
+  base$knitr$opts_chunk$fig.width <- 4.9 # 6.125" * 0.8, as in template
+  base$knitr$opts_chunk$fig.height <- 3.675 # 4.9 * 3:4
+  base$knitr$opts_chunk$fig.align <- "center"
+
+  hook_chunk <- function(x, options) {
     if (knitr:::output_asis(x, options)) return(x)
-    paste('\\begin{Schunk}\n', x, '\\end{Schunk}', sep = '')
+    paste0('\\begin{CodeChunk}\n', x, '\\end{CodeChunk}')
   }
-  hook_input <- function(x, options)
-    paste(c('\\begin{Sinput}', knitr:::hilight_source(x, 'sweave', options), '\\end{Sinput}', ''),
-      collapse = '\n')
-  hook_output <- function(x, options) paste('\\begin{Soutput}\n', x, '\\end{Soutput}\n', sep = '')
+  hook_input <- function(x, options) {
+    paste0(c('\\begin{CodeInput}', x, '\\end{CodeInput}', ''),
+           collapse = '\n')
+  }
+  hook_output <- function(x, options) {
+    paste0('\\begin{CodeOutput}\n', x, '\\end{CodeOutput}\n')
+  }
 
   base$knitr$knit_hooks$chunk   <- hook_chunk
   base$knitr$knit_hooks$source  <- hook_input
@@ -39,34 +36,4 @@ reed_thesis <- function() {
   base$knitr$knit_hooks$plot <- knitr:::hook_plot_tex
 
   base
-}
-
-#' Render a pandoc template.
-#'
-#' This is a hacky way to access the pandoc templating engine.
-#'
-#' @param metadata A named list containing metadata to pass to template.
-#' @param template Path to a pandoc template.
-#' @param output Path to save output.
-#' @return (Invisibly) The path of the generate file.
-#' @examples
-#' x <- rticles:::template_pandoc(
-#'   list(preamble = "%abc", filename = "wickham"),
-#'   rticles:::find_resource("rjournal_article", "RJwrapper.tex"),
-#'   tempfile()
-#' )
-#' if (interactive()) file.show(x)
-template_pandoc <- function(metadata, template, output, verbose = FALSE) {
-  tmp <- tempfile(fileext = ".md")
-  on.exit(unlink(tmp))
-
-  cat("---\n", file = tmp)
-  cat(yaml::as.yaml(metadata), file = tmp, append = TRUE)
-  cat("---\n", file = tmp, append = TRUE)
-  cat("\n", file = tmp, append = TRUE)
-
-  rmarkdown::pandoc_convert(tmp, "markdown", output = output,
-    options = paste0("--template=", template), verbose = verbose)
-
-  invisible(output)
 }
