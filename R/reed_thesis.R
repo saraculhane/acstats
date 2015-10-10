31de5dae2842337e976e352f5a998d351deac2fd
Chester Ismay <chester.ismay@gmail.com>
2015-10-10 12:11:34 -0700

    Abstract included now

--- R/reed_thesis.R
+++ R/reed_thesis.R
@@ -2,7 +2,7 @@
 reed_thesis <- function() {
   template <- find_resource("reed_thesis", "template.tex")
 
-  base <- rmarkdown::pdf_document(template = template, toc = FALSE, keep_tex = TRUE)
+  base <- rmarkdown::pdf_document(template = template, toc = toc, keep_tex = TRUE)
 
   # Mostly copied from knitr::render_sweave
   base$knitr$opts_knit$out.format <- "sweave"
