5cfe3de6f3e21f7ef8ddec977b2fc14eadf3160e
Chester Ismay <chester.ismay@gmail.com>
2015-10-25 12:31:09 -0700

    Commented out hook specifications

--- R/reed_thesis.R
+++ R/reed_thesis.R
@@ -20,23 +20,23 @@ reed_thesis <- function(toc = TRUE) {
   base$knitr$opts_chunk$fig.height <- 3.675 # 4.9 * 3:4
   base$knitr$opts_chunk$fig.align <- "center"
 
-  hook_chunk <- function(x, options) {
-    if (knitr:::output_asis(x, options)) return(x)
-    paste0('\\begin{CodeChunk}\n', x, '\\end{CodeChunk}')
-  }
-  hook_input <- function(x, options) {
-    paste0(c('\\begin{Shaded}', x, '\\end{Shaded}', ''),
-           collapse = '\n')
-  }
-  hook_output <- function(x, options) {
-    paste0('\\begin{CodeOutput}\n', x, '\\end{CodeOutput}\n')
-  }
-
-  base$knitr$knit_hooks$chunk   <- hook_chunk
-  base$knitr$knit_hooks$source  <- hook_input
-  base$knitr$knit_hooks$output  <- hook_output
-  base$knitr$knit_hooks$message <- hook_output
-  base$knitr$knit_hooks$warning <- hook_output
+#  hook_chunk <- function(x, options) {
+#    if (knitr:::output_asis(x, options)) return(x)
+#    paste0('\\begin{CodeChunk}\n', x, '\\end{CodeChunk}')
+#  }
+#  hook_input <- function(x, options) {
+#    paste0(c('\\begin{Shaded}', x, '\\end{Shaded}', ''),
+#           collapse = '\n')
+#  }
+#  hook_output <- function(x, options) {
+#    paste0('\\begin{CodeOutput}\n', x, '\\end{CodeOutput}\n')
+#  }
+#
+#  base$knitr$knit_hooks$chunk   <- hook_chunk
+#  base$knitr$knit_hooks$source  <- hook_input
+#  base$knitr$knit_hooks$output  <- hook_output
+#  base$knitr$knit_hooks$message <- hook_output
+#  base$knitr$knit_hooks$warning <- hook_output
   base$knitr$knit_hooks$plot <- knitr:::hook_plot_tex
 
   base
